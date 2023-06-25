90732a04cc70d290e5983dd853b63a6572997374
Bugra.Akman <akmanbugra@gmail.com>
2023-06-25 18:04:44 +0300

    Front-end complete in html.

--- myapp/src/Pages/Todo.jsx
+++ myapp/src/Pages/Todo.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function Todo() {
     return (
         <div className='todoAppArea'>
-            <FontAwesomeIcon icon={faPlus} />
+            <header>Todo App</header>
+            <div className="inputField">
+                <input type="text" placeholder='Add your new todo' />
+                <button><FontAwesomeIcon icon={faPlus} /></button>
+            </div>
+            <ul className="todoList">
+                <li>Buy a new apple macbook pro <span><FontAwesomeIcon icon={faTrash} /></span></li>
+                <li>Complate a previous task <span><FontAwesomeIcon icon={faTrash} /></span></li>
+                <li>Create a video for YouTube <span><FontAwesomeIcon icon={faTrash} /></span></li>
+                <li>Create a new portfolio site <span><FontAwesomeIcon icon={faTrash} /></span></li>
+            </ul>
+            <div className="footer">
+                <span>You have 4 pending tasks</span>
+                <button>Clear All</button>
+            </div>
         </div>
     )
 }
