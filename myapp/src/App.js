3902a31c1fba736572f93b0c93baf30ad3b2d240
Bugra.Akman <akmanbugra@gmail.com>
2023-06-26 11:47:36 +0300

    Converted css to sass.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+// import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Todo from './Pages/Todo';
+import './Pages/Todo.sass';
 
 function App() {
   return (
